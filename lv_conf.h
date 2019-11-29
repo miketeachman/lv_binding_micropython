cf98a0f655d25dcf6041ae56c4d7a70a026f1b40
Mike Teachman <mike.teachman@gmail.com>
2019-11-28 21:23:27 -0800

    added monospace fonts

--- lv_conf.h
+++ lv_conf.h
@@ -333,7 +333,12 @@ typedef void * lv_indev_drv_user_data_t;            /*Type of user data in the i
  *                                LV_FONT_DECLARE(my_font_2)
  */
 #ifndef LV_FONT_CUSTOM_DECLARE
-#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(lv_font_roboto_80) \
+#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(lv_font_roboto_mono_40) \
+                               LV_FONT_DECLARE(lv_font_roboto_mono_50) \
+                               LV_FONT_DECLARE(lv_font_roboto_mono_60) \
+                               LV_FONT_DECLARE(lv_font_roboto_mono_70) \
+                               LV_FONT_DECLARE(lv_font_roboto_mono_80) \
+                               LV_FONT_DECLARE(lv_font_roboto_mono_100) \
                                LV_FONT_DECLARE(lv_font_roboto_120)
 #endif // LV_FONT_CUSTOM_DECLARE
 
